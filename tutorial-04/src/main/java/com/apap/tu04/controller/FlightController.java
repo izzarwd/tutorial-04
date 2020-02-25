6cf6191decd0734578741a666ce75b00daabb1d6
Izzatunnisa Arwadi <nisarwd@gmail.com>
2020-02-25 16:15:37 +0700

    Tutorial 04 versi 1

---
+++ tutorial-04/src/main/java/com/apap/tu04/controller/FlightController.java
@@ -0,0 +1,61 @@
+package com.apap.tu04.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.apap.tu04.model.FlightModel;
+import com.apap.tu04.model.PilotModel;
+import com.apap.tu04.service.FlightService;
+import com.apap.tu04.service.PilotService;
+
+/**
+ * FlightController
+ */
+@Controller
+public class FlightController {
+	@Autowired
+	private FlightService flightService;
+	
+	@Autowired
+	private PilotService pilotService;
+	
+	@RequestMapping(value = "/flight/add/{licenseNumber}", method = RequestMethod.GET)
+	private String add(@PathVariable(value = "licenseNumber") String licenseNumber, Model model) {
+		FlightModel flight = new FlightModel();
+		PilotModel pilot = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+		flight.setPilot(pilot);
+		model.addAttribute("flight", flight);
+		return "addFlight";
+	}
+	
+	@RequestMapping(value = "/flight/add", method = RequestMethod.POST)
+	private String addFlightSubmit(@ModelAttribute FlightModel flight) {
+		flightService.addFlight(flight);
+		return "add";
+	}
+	
+	@RequestMapping("/flight/delete/{flightNumber}")
+	private String delete(@PathVariable(value = "flightNumber") String flightNumber) {
+		flightService.deleteFlight(flightNumber);
+		return "delete";
+	}
+	
+	@RequestMapping(value= "/flight/edit/{flightNumber}", method = RequestMethod.GET)
+	public String edit(@PathVariable(value = "flightNumber") String flightNumber, Model model) {
+		FlightModel archive = flightService.getFlightDetailByflightNumber(flightNumber);
+		model.addAttribute("flight", archive);
+	    return "editFlight";
+	}
+	
+	@RequestMapping(value = "/flight/edit", method = RequestMethod.POST)
+	private String editFlightSubmit(@ModelAttribute FlightModel flight, Long id) {
+		flightService.addFlight(flight);
+		return "update";
+	}
+	
+}
---
+++ tutorial-04/src/main/java/com/apap/tu04/service/FlightService.java
@@ -0,0 +1,12 @@
+package com.apap.tu04.service;
+
+import com.apap.tu04.model.FlightModel;
+
+/**
+ * FlightService
+ */
+public interface FlightService {
+	void addFlight(FlightModel flight);
+	void deleteFlight(String flightNumber);
+	FlightModel getFlightDetailByflightNumber(String flightNumber);
+}
